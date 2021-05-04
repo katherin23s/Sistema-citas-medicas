8fd2ed2d1979b47ef694f70f4570a4187aadd2b2
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-05-03 19:44:50 -0700

    Crear pacientes diseño, validaciones, campos adecuados.

--- resources/views/pages/ventanas/AltaPaciente.blade.php
+++ resources/views/pages/ventanas/AltaPaciente.blade.php
@@ -16,165 +16,176 @@
 
         </style>
 
-        <div class="row ml-2">
-            <h4 id="minimum-setup">Patients Information</h4>
-            <div class="container-fluid p-0">
-                <form action="{{ url('/pacientes') }}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <!-- paciente y medico nombre -->
-                    <div class="m-0 px-5">
-
-                        <div class="row">
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label">Nombre</label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="nombre" name="nombre">
-
-                                    </div>
-                                </div>
-                            </div>
+        <div class="row m-1 ">
+            <div class="col">
+                <hr class="borde">
+            </div>
+            <h3 class="col-auto minimum-setup text-primary" id="minimum-setup">Patients</h3>
+            <div class="col">
+                <hr class="borde">
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12 grid-margin p-4">
+                        <!-- Buscador y Boton Agregar Responsivo -->
+                        <div class="bg-primary text-center text-white m-0 p-0">
+                            Patients Information
                         </div>
+                        <div class="card">
+                            <div class="card-body pt-0">
+                                <!-- FORMULARIO-->
 
-                        <div class="row">
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label">Apellido Parterno</label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="apellido" name="apellido">
-                                    </div>
-                                </div>
-                            </div>
+                                <!----===============1. Campos de busqueda y añadir nueva cita FORM ==================--------->
+                                <h4 id="minimum-setup" class="text-primary">Patients Information</h4>
+                                <form action="{{ url('/pacientes') }}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    <!-- paciente y medico nombre -->
+                                    <div class="m-0 px-5">
 
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label">Apellido Materno</label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="apellidoM" name="apellidoM">
+                                        <div class="row">
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleInputEmail1" class="col-form-label">Nombre</label>
+                                                    <div class="col input-group mb-3">
+                                                        <input class="form-control border-0" style="background:#f7f7f7"
+                                                            type="text" id="nombre" name="nombre" required>
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div class="row">
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label">Direccion</label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="direccion" name="direccion">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-12 col-sm-6">
-                                <!-- Fecha -->
-                                <div class="">
-                                    <div class="form-group row">
-                                        <label for="example-date-input" class="col-form-label">Registro</label>
-                                        <div class="col input-group mb-3">
-                                            <input class="form-control" type="date" id="registro" name="registro">
+                                            <div class="col-12 col-sm-6">
+                                                <!-- Fecha -->
+                                                <div class="">
+                                                    <div class="form-group row">
+                                                        <label for="example-date-input" class="col-form-label">Date</label>
+                                                        <div class="col input-group mb-3">
+                                                            <input class="form-control  border-0" style="background:#f7f7f7"
+                                                                type="date" id="fechaNacimiento" name="fechaNacimiento"
+                                                                required>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label">Teléfono</label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="telefono" name="telefono">
-                                    </div>
-                                </div>
-                            </div>
+                                        <div class="row">
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleInputEmail1" class="col-form-label">Apellido
+                                                        Parterno</label>
+                                                    <div class="col input-group mb-3">
+                                                        <input class="form-control border-0" type="text" id="apellido"
+                                                            name="apellido" style="background:#f7f7f7" required>
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label">Edad</label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="edad" name="edad">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label">Email</label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="email" name="email">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group row">
-                                    <label for="exampleInputEmail1" class="col-form-label pr-4">Sexo </label>
-                                    <div class="col input-group mb-3">
-                                        <input class="form-control" id="sexo" name="sexo">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-12 col-sm-6">
-                                <!-- Fecha -->
-                                <div class="">
-                                    <div class="form-group row">
-                                        <label for="example-date-input" class="col-form-label">Date</label>
-                                        <div class="col input-group mb-3">
-                                            <input class="form-control" type="date" id="fechaNacimiento"
-                                                name="fechaNacimiento">
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleInputEmail1" class="col-form-label">Teléfono</label>
+                                                    <div class="col input-group mb-3">
+                                                        <input class="form-control border-0" id="telefono" name="telefono"
+                                                            style="background:#f7f7f7">
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12 col-sm-6">
-                                <!-- Tiempo de duracion -->
-                                <div class="">
-                                    <div class="form-group row">
-                                        <label for="exampleSelect1" class="col-form-label">Status</label>
-                                        <div class="col input-group mb-3">
-                                            <input class="form-control" type="status" id="status" name="status">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
 
-                        <div class="row">
-                            <div class="col-12 col-sm-12">
-                                <!-- Tiempo de duracion -->
-                                <div class="">
-                                    <div class="form-group row">
-                                        <label for="exampleSelect1" class="col-form-label">Activo</label>
-                                        <div class="col input-group mb-3">
-                                            <select class="form-control" id="activo" name="activo">
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
+
+                                        <div class="row">
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleInputEmail1" class="col-form-label">Apellido
+                                                        Materno</label>
+                                                    <div class="col input-group mb-3">
+                                                        <input class="form-control border-0" type="text" id="apellidoM"
+                                                            name="apellidoM" style="background:#f7f7f7" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleInputEmail1" class="col-form-label">Email</label>
+                                                    <div class="col input-group mb-3">
+                                                        <input class="form-control border-0" type="email" id="email"
+                                                            name="email" style="background:#f7f7f7" required>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
+
+                                        <div class="row">
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleSelect1" class="col-form-label">Sexo</label>
+                                                    <div class="col input-group mb-3">
+                                                        <select class="form-control border-0" id="sexo" name="sexo"
+                                                            style="background:#f7f7f7">
+                                                            <option>Mujer</option>
+                                                            <option>Hombre</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleInputEmail1" class="col-form-label">Direccion</label>
+                                                    <div class="col input-group mb-3">
+                                                        <input class="form-control border-0" id="direccion" name="direccion"
+                                                            style="background:#f7f7f7">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-12 col-sm-6">
+                                                <div class="form-group row">
+                                                    <label for="exampleInputEmail1" class="col-form-label">Edad</label>
+                                                    <div class="col input-group mb-3">
+                                                        <input class="form-control border-0" id="edad" name="edad"
+                                                            style="background:#f7f7f7">
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12 col-sm-6">
+                                                <!-- Fecha -->
+                                                <div class="">
+                                                    <div class="form-group row">
+                                                        <label for="example-date-input"
+                                                            class="col-form-label">Registro</label>
+                                                        <div class="col input-group mb-3">
+                                                            <input class="form-control border-0" type="date" id="registro"
+                                                                style="background:#f7f7f7" name="registro" required>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <input type="submit" id="Enviar">
                                     </div>
-                                </div>
+                                </form>
                             </div>
+                            <!-- FIN FORMULARIO -->
                         </div>
-                        <input type="submit" id="Enviar">
                     </div>
-                </form>
+                </div>
+
+
             </div>
         </div>
     </div>
     </div>
+    </div>
+
+
+
+
 
 @endsection
