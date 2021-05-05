3edb20e144816133447f9c1ac413e00e0662bd74
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-05-05 11:38:46 -0700

    Acomodar usuarios vista.

--- resources/views/pages/administrador/usuarios.blade.php
+++ resources/views/pages/administrador/usuarios.blade.php
@@ -17,145 +17,28 @@
         </style>
         <!----===============1. Campos de busqueda y añadir nueva cita FORM ==================--------->
         <!----------------------------------------------------------------------------------------------------------->
+
         <!-- Buscador y Boton Agregar Responsivo -->
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="row">
                     <div class="col-md-12 grid-margin p-4">
-                        <div class="card">
+                        <div class="card p-4">
 
                             <!---------- Division lineal de busqueda y agregar nuevas citas -------->
-                            <div class="row m-1 ">
-                                <div class="col">
-                                    <hr class="borde">
-                                </div>
-                                <div class="col-auto">USUARIOS</div>
-                                <div class="col">
-                                    <hr class="borde">
-                                </div>
+                            <!-- Agregar Seleccionado -->
+
+                            <h2 class="text-center">Usuarios</h2>
+                            <div class="row mb-2 mx-3 p-0">
+                                <a type="button" href="{{ url('usuarios/create') }}" id="añadir"
+                                    class="btn btn-primary col-12 col-xl-1 mr-auto text-left">Añadir seleccionado</a>
                             </div>
 
                             <!-- -->
-
-                            <div class="card-body pt-0">
-                                <div class="d-flex justify-content-between">
-                                    <h4 class="card-title mb-0">Citas</h4>
-                                    <a href="#"><small>Citas</small></a>
-                                </div>
-
-
-
-                                <div class="container">
-                                    <h2>Laravel AJAX Autocomplete Demo</h2>
-                                    <select class="autosearch form-control" name="autosearch"
-                                        id="js-example-templating"></select>
-                                </div>
-                                <!-- Buscador y Boton Agregar Responsivo -->
-
-                                <div class="row m-0 d-flex">
-                                    <div class="card col-12 col-xl-3 mt-3 mr-3 mb-3 ">
-                                        <form class="card-body pb-2" action="{{ route('usuarios.index') }}" method="get">
-                                            <div class="input-group mb-3">
-                                                <input type="text" class="form-control" name="buscar" placeholder="Browser"
-                                                    aria-label="Browser" aria-describedby="basic-addon2">
-                                                <div class="input-group-append d-flex justify-content-end">
-                                                    <button class="btn btn-outline-primary" type="button">Buscar</button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    <!-- Agregar Seleccionado -->
-                                    <a type="button" href="{{ url('usuarios/create') }}" id="añadir"
-                                        class="btn btn-primary col-12 col-xl-1 my-5 mr-auto ">Añadir
-                                        seleccionado</a>
-                                    <!-- -->
-
-                                    <!-- CRUD de botones -->
-                                    <div class="row d-flex justify-content-end m-0 pr-5 border-0 ">
-                                        <div class="btn-group border-0" role="group" aria-label="Basic example">
-                                            <button type="button" class="btn btn-primary my-5 p-3" id="agregar"
-                                                data-toggle="modal" data-target="#altaModal">Altas</button>
-                                            <button type="button" data-toggle="modal" data-target="#modificarModal"
-                                                class="btn btn-success my-5">Modificacion</button>
-                                            <button type="button" class="btn btn-danger my-5" data-toggle="modal"
-                                                data-target="#eliminarModal">Eliminacion</button>
-                                        </div>
-                                    </div>
-                                    <!-- -->
-
-                                    <div class="form-group col-2 mt-4 p-0">
-                                        <label for="exampleFormControlSelect1">Show</label>
-                                        <select class="form-control form-control-lg" id="exampleFormControlSelect1">
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
-                                        </select>
-                                    </div>
-
-                                </div>
-
-                                <div class="m-4">
-                                    <input id="search" type="text" class="form-control form-control-lg" name="buscar"
-                                        aria-label="Large" placeholder="Browser" aria-describedby="basic-addon2">
-                                </div>
-
-                                <!-- -->
-                                <div class="table-responsive">
-                                    <table class="table table-striped table-hover">
-                                        <thead class="bg-primary">
-                                            <tr>
-                                                <th>Id</th>
-                                                <th>Nombre</th>
-                                                <th>Apellido</th>
-                                                <th>Apellido Materno</th>
-                                                <th>direccion</th>
-                                                <th>telefono</th>
-                                                <th>email</th>
-                                                <th>sexo</th>
-                                                <th>Acciones</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($usuarios as $usuario)
-                                                <tr id="idtr" action="{{ $usuario->id }}">
-                                                    <td id=" idCita" type="hidden" style="display:none;">
-                                                        {{ $usuario->id }}</td>
-                                                    <td>{{ ++$i }}</td>
-                                                    <td>{{ $usuario->name }}</td>
-                                                    <td>{{ $usuario->apellido }}</td>
-                                                    <td>{{ $usuario->apellidoM }}</td>
-                                                    <td>{{ $usuario->direccion }}</td>
-                                                    <td>{{ $usuario->telefono }}</td>
-                                                    <td>{{ $usuario->email }}</td>
-                                                    <td>{{ $usuario->sexo }}</td>
-                                                    <td>
-                                                        <a href="{{ url('usuarios/' . $usuario->id . '/edit') }}"
-                                                            type="submit" class="btn btn-success">
-                                                            Modificar
-                                                        </a>
-                                                        <form action="{{ url('usuarios/' . $usuario->id) }}"
-                                                            method="POST">
-                                                            @csrf
-                                                            {{ method_field('DELETE') }}
-                                                            <input type="submit" class="btn btn-danger"
-                                                                onclick="return confirm('quieres borrar?')"
-                                                                value="   Borrar   ">
-                                                        </form>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div class="row d-flex justify-content-end p-1">
-                                    <div class="btn-group mr-2" role="group" aria-label="First group">
-                                        {!! $usuarios->links() !!}
-                                    </div>
-                                </div>
+                            <div class="container-fluid">
+                                <select class="autosearch form-control pr-1" name="autosearch"
+                                    id="js-example-templating"></select>
                             </div>
-
                             <!---------- /Division lineal de busqueda y agregar nuevas citas -------->
 
                             <style>
@@ -176,65 +59,51 @@
             </div>
         </div>
 
+    </div>
+    </div>
+    <script type="text/javascript">
+        let ruta = "dash-board/assets/images/usuarios/foto.png";
+        $('.autosearch').select2({
+            placeholder: 'Usuarios',
+            templateResult: formatState,
+            closeOnSelect: false,
+            ajax: {
+                url: 'buscar-usuarios-auto',
+                dataType: 'json',
+                delay: 220,
+                processResults: function(data) {
+                    return {
+                        results: $.map(data, function(data) {
+                            return {
+                                text: data.name + " " + data.apellido + " " + data.apellidoM,
+                                id: data.id,
+                            }
+                        })
+                    };
+                },
+                //   templateResult: formatData,
+                cache: true
+            }
+        });
 
 
 
-        <div class="container">
-            <h2>Laravel AJAX Autocomplete Demo</h2>
-            <select class="autosearch form-control" name="autosearch"></select>
-        </div>
+        /******************************************************************************************************/
 
-        <div>
-            <select id="cmbIdioma" style="width: 200px;">
-                <option value="Spain" selected>Spain</option>
-                <option value="United_Kingdom">United Kingdom</option>
-            </select>
-        </div>
+        function formatState(state) {
+            if (!state.id) {
+                return state.text;
+            }
+            var $state = $(
+                '<div class="row m-0 p-0 font-weight-light"> <div class="col-auto mr-auto"> <span><img src="dash-board/assets/images/usuarios/foto.png" class="img-flag" width="50" height="50"/>' +
+                " " + state.text + ' </span></div>  <div class="col-auto"> <a href="usuarios/' + state.id +
+                '/edit" type="submit" class="btn btn-success">Modificar' +
+                '</a>  <a href="usuarios/' + state.id +
+                '/edit" type="submit" class="btn btn-danger">Eliminar </a> </div></div>'
+            );
+            return $state;
+        };
+        /*************************************************************************/
 
-
-        <script type="text/javascript">
-            let ruta = "dash-board/assets/images/usuarios/foto.png";
-            $('.autosearch').select2({
-                placeholder: 'Select Books',
-                templateResult: formatState,
-                ajax: {
-                    url: 'buscar-usuarios-auto',
-                    dataType: 'json',
-                    delay: 220,
-                    processResults: function(data) {
-                        return {
-                            results: $.map(data, function(data) {
-                                return {
-                                    text: data.name + " " + data.apellido + " " + data.apellidoM,
-                                    id: data.id,
-
-                                }
-                            })
-                        };
-                    },
-                    //   templateResult: formatData,
-                    cache: true
-                }
-            });
-
-
-
-            /******************************************************************************************************/
-
-            function formatState(state) {
-                if (!state.id) {
-                    return state.text;
-                }
-                var $state = $(
-                    '<div class="row m-0 p-0"> <div class="col-auto mr-auto"> <span><img src="dash-board/assets/images/usuarios/foto.png" class="img-flag" width="50" height="50"/>' +
-                    " " + state.text + ' </span></div>  <div class="col-auto"> <a href="usuarios/' + state.id +
-                    '/edit" type="submit" class="btn btn-success">Modificar' +
-                    '</a>  <a href="usuarios/' + state.id +
-                    '/edit" type="submit" class="btn btn-danger">Eliminar </a> </div></div>'
-                );
-                return $state;
-            };
-            /*************************************************************************/
-
-        </script>
-    @endsection
+    </script>
+@endsection
