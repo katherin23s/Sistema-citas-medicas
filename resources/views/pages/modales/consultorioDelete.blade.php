9326398693c9d5c87578b8d10eb966dfbaa1b40e
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-05-06 14:55:06 -0700

    Cerrar boton modales consultorios.

--- resources/views/pages/modales/consultorioDelete.blade.php
+++ resources/views/pages/modales/consultorioDelete.blade.php
@@ -1,31 +1,32 @@
 <!--------------------------------------------- Modal eliminar --------------------------------------->
 <!-- Modal medico eliminar -->
-<div class="modal fade" id="modal-delete-consultorio-{{$consultorio->idConsultorio}}" tabindex="-1" role="dialog" aria-labelledby="eliminarModal" aria-hidden="true">
-  <form action="{{route('consultorios.destroy',$consultorio->idConsultorio)}}" method="POST" id="deleteForm">
-      @csrf
-      {{method_field('DELETE')}}
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-          <h4 class="modal-title" id="myModalLabel">Eliminar medico</h4>
-         
-              <div class="modal-body">
-               
-                  <p class="text-center">
-                  Are you sure you want to delete this #{{$consultorio->noConsultorio}}?
-                  </p>
-              </div>
-  
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-        <input type="submit" name="_method" id="idConsultorio" value="DELETE">
-      </div>
+<div class="modal fade" id="modal-delete-consultorio-{{ $consultorio->idConsultorio }}" tabindex="-1" role="dialog"
+    aria-labelledby="eliminarModal" aria-hidden="true">
+    <form action="{{ route('consultorios.destroy', $consultorio->idConsultorio) }}" method="POST" id="deleteForm">
+        @csrf
+        {{ method_field('DELETE') }}
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">Eliminar medico</h4>
 
-    </div>
-  </div>
-</form>
+                    <div class="modal-body">
+
+                        <p class="text-center">
+                            Are you sure you want to delete this #{{ $consultorio->noConsultorio }}?
+                        </p>
+                    </div>
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    <input type="submit" name="_method" id="idConsultorio" value="DELETE" data-dismiss="modal">
+                </div>
+
+            </div>
+        </div>
+    </form>
 </div>
--- resources/views/pages/modales/consultorioUpdate.blade.php
+++ resources/views/pages/modales/consultorioUpdate.blade.php
@@ -1,75 +1,85 @@
 <div class="container">
     <!------------------------------------- Modal para modificar ------------------------------------------->
-    <div class="modal fade" id="modal-update-consultorio-{{$consultorio->idConsultorio}}" tabindex="-1" role="dialog" aria-labelledby="modificarModal" aria-hidden="true">
-       <div class="modal-dialog modal-lg modal-dialog-centered">
-         <div class="modal-content">
-           <div class="modal-header">
-             <h4 class="modal-title" id="myModalLabel">Basic Modal {{$consultorio->idConsultorio}}</h4>
-             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-               <span aria-hidden="true">&times;</span>
-             </button>
-           </div>
-        <div class="modal-body">
-            <form id="myFormModificar" name="myFormModificar" class="form-horizontal" novalidate="">
-            <!----===============1. Campos de busqueda y añadir nueva cita FORM ==================--------->
-           <div class="row ml-2 ">
-            <h4 id="minimum-setup">Doctors Information</h4>
-            <div class="container-fluid p-0">
-                <div class="row m-1 ">
-                    <div class="col">
-                        <hr class="borde">
-                    </div>
-                    <h3 class="col-auto minimum-setup" id="minimum-setup">Consultorio</h3>
-                    <div class="col">
-                        <hr class="borde">
-                    </div>
+    <div class="modal fade" id="modal-update-consultorio-{{ $consultorio->idConsultorio }}" tabindex="-1" role="dialog"
+        aria-labelledby="modificarModal" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">Basic Modal {{ $consultorio->idConsultorio }}</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
+                <div class="modal-body">
+                    <form id="myFormModificar" name="myFormModificar" class="form-horizontal" novalidate="">
+                        <!----===============1. Campos de busqueda y añadir nueva cita FORM ==================--------->
+                        <div class="row ml-2 ">
+                            <h4 id="minimum-setup">Doctors Information</h4>
+                            <div class="container-fluid p-0">
+                                <div class="row m-1 ">
+                                    <div class="col">
+                                        <hr class="borde">
+                                    </div>
+                                    <h3 class="col-auto minimum-setup" id="minimum-setup">Consultorio</h3>
+                                    <div class="col">
+                                        <hr class="borde">
+                                    </div>
+                                </div>
 
-                <!-- paciente y medico nombre -->
-                <div class="m-0 px-5">
-                    <div class="row">
-                        <div class="col-12 col-sm-6">
-                            <div class="form-group row">
-                                <label for="exampleInputEmail1" class="col-form-label">No Consultorio</label>
-                                <div class="col input-group mb-3">
-                                    <input class="form-control" id="noConsultorio{{$consultorio->idConsultorio}}" name="noConsultorio" value="{{$consultorio->noConsultorio}}">
+                                <!-- paciente y medico nombre -->
+                                <div class="m-0 px-5">
+                                    <div class="row">
+                                        <div class="col-12 col-sm-6">
+                                            <div class="form-group row">
+                                                <label for="exampleInputEmail1" class="col-form-label">No
+                                                    Consultorio</label>
+                                                <div class="col input-group mb-3">
+                                                    <input class="form-control"
+                                                        id="noConsultorio{{ $consultorio->idConsultorio }}"
+                                                        name="noConsultorio" value="{{ $consultorio->noConsultorio }}">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-sm-6">
+                                            <div class="form-group row">
+                                                <label for="exampleInputEmail1" class="col-form-label">Status</label>
+                                                <div class="col input-group mb-3">
+                                                    <input class="form-control"
+                                                        id="status{{ $consultorio->idConsultorio }}" name="status"
+                                                        value="{{ $consultorio->status }}">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <!-- Tiempo de duracion -->
+                                        <div class="col-12 col-sm-6">
+                                            <div class="form-group row">
+                                                <label class="col-form-label">Status</label>
+                                                <div class="col input-group mb-3">
+                                                    <input class="form-control"
+                                                        id="activo{{ $consultorio->idConsultorio }}" name="activo"
+                                                        value="{{ $consultorio->activo }}">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- cierre -->
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-sm-6">
-                            <div class="form-group row">
-                                <label for="exampleInputEmail1" class="col-form-label">Status</label>
-                                <div class="col input-group mb-3">
-                                    <input class="form-control" id="status{{$consultorio->idConsultorio}}" name="status" value="{{$consultorio->status}}">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-           
-                <div class="row">
-                    <!-- Tiempo de duracion -->
-                    <div class="col-12 col-sm-6">
-                        <div class="form-group row">
-                            <label  class="col-form-label">Status</label>
-                            <div class="col input-group mb-3">
-                                <input class="form-control" id="activo{{$consultorio->idConsultorio}}" name="activo" value="{{$consultorio->activo}}">
-                            </div>
-                        </div>
-                    </div>
+                        <!-- -->
+
+                    </form>
                 </div>
-                <!-- cierre -->
-            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="btn-update{{ $consultorio->idConsultorio }}"
+                        data-id="{{ $consultorio->idConsultorio }}" onclick="modificar(this)" data-dismiss="modal">Save
+                        changes</button>
+                </div>
             </div>
         </div>
-        <!-- -->
-          
-        </form>
     </div>
-           <div class="modal-footer">
-             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-             <button type="button" class="btn btn-primary" id="btn-update{{ $consultorio->idConsultorio}}" data-id="{{ $consultorio->idConsultorio}}" onclick="modificar(this)">Save changes</button>
-           </div>
-         </div>
-       </div>
-     </div>
-</div> 
+</div>
