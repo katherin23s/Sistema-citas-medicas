633a5aa29337040bce0684d6254d2920b68388f6
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-05-07 16:06:10 -0700

    Cierre de sesión de Dashboard laravel y acomodar y función.

--- resources/views/indexDashboard.blade.php
+++ resources/views/indexDashboard.blade.php
@@ -45,6 +45,14 @@
     <link rel="shortcut icon" href="{{ asset('dash-board/assets') }}/images/favicon.ico" />
 
 
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
